2444b8e6f33ea27f73dde7b06978c4ab83ace0b7
Belyga9000 <vlcode93@gmail.com>
2022-05-11 20:31:34 +0300

    added sorting of products as well

--- shop/src/components/Content/ContentContainer.js
+++ shop/src/components/Content/ContentContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectedCategory } from "../../store/header/headerSlice";
+import {
+  filteredProducts,
+  selectedCategory,
+} from "../../store/header/headerSlice";
 import { Content } from "./Content";
 
 export const ContentContainer = () => {
   const category_name = useSelector(selectedCategory);
+  const products = useSelector(filteredProducts);
 
-  return <Content category_name={category_name} />;
+  return <Content products={products} category_name={category_name} />;
 };
